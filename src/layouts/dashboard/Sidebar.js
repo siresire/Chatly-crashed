5f440b763a8779872d5c8510e5e9c30145355f54
Godfrey Bosire <godfreybosire@Godfreys-MacBook-Pro.local>
2023-10-09 21:43:37 +0300

    font-end_done

--- src/layouts/dashboard/Sidebar.js
+++ src/layouts/dashboard/Sidebar.js
@@ -5,160 +5,201 @@ import React from "react";
 import { useState } from "react";
 import { faker } from '@faker-js/faker';
 import Logo from "../../assets/Images/logo.ico";
-import { Nav_Buttons } from "../../data";
+import { Nav_Buttons, Nav_Setting } from "../../data";
 import useSettings from "../../hooks/useSettings";
 import AntSwitch from "../../components/AntSwitch";
 import { Profile_Menu } from "../../data"
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+// import { UpdateTab } from "../../redux/slices/app";
+// import ProfileMenu from "./ProfileMenu";
+
 
 
 const getPath = (index) => {
   switch (index) {
     case 0:
       return "/app";
+
     case 1:
       return "/group";
+
     case 2:
       return "/call";
+
     case 3:
       return "/settings";
+
     default:
-      return "/404";
+      break;
+  }
+};
+
+const getMenuPath = (index) => {
+
+  switch (index) {
+    case 0:
+      return "/profile";
+
+    case 1:
+      return "/settings";
+
+    case 2:
+      return "/auth/login";
+
+    default:
+      break;
   }
 
 };
 
-const Sidebar = () =>{
-    const theme = useTheme();
-    const navigate = useNavigate();
-    const [selected, setSelected] = useState(0);
-    const { onToggleMode } = useSettings();
 
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-        
-        
-    };
-    const handleClose = () => { setAnchorEl(null); };
-    return (
-        <Box
-        p={2}
-        sx={{
-          backgroundColor: theme.palette.background.paper,
-          boxShadow: "0px 0px 2px rgba(0,0,0,0.25)",
-          width: 100,
-        }}
-      >
-        <Stack direction="column" alignItems={"center"} justifyContent={"space-between"} sx={{ height: "100%" }} spacing={3}>
-          <Stack alignItems={"center"} spacing={3}>
-            <Box
-              sx={{
-                backgroundColor: theme.palette.primary.main,
-                height: 64,
-                width: 64,
-                borderRadius: 1.5,
-              }}
-            >
-              <img src={Logo} alt={"Chat app logo"} />
-            </Box>
-            <Stack sx={{ width: "max-content" }} direction="column" alignItems={"center"} spacing={3}>
-              {Nav_Buttons.map((el, index) =>
-                el.index === selected ? (
-                  <Box key={index} sx={{ backgroundColor: theme.palette.primary.main, borderRadius: 1.5 }}>
-                    <IconButton sx={{ width: "max-content", color: "#fff" }} key={el.index}>
-                      {el.icon}
-                    </IconButton>
-                  </Box>
-                ) : (
-                  <IconButton
-                    onClick={() => {
-                      setSelected(el.index);
-                      navigate(getPath(el.index));
-                      
-                    }}
-                    sx={{
-                      width: "max-content",
-                      color: theme.palette.mode === "Light" ? "#000" : theme.palette.text.primary,
-                    }}
-                    key={el.index}
-                  >
+const Sidebar = () => {
+  const theme = useTheme();
+  const dispatch = useDispatch();
+
+  const { tab } = useSelector((state) => state.app);
+
+  const navigate = useNavigate();
+
+  const [selected, setSelected] = useState(0);
+  const { onToggleMode } = useSettings();
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+
+  };
+  const handleClose = () => { setAnchorEl(null); };
+
+
+  return (
+    <Box
+      p={2}
+      sx={{
+        backgroundColor: theme.palette.background.paper,
+        boxShadow: "0px 0px 2px rgba(0,0,0,0.25)",
+        width: 100,
+      }}
+    >
+      <Stack direction="column" alignItems={"center"} justifyContent={"space-between"} sx={{ height: "100%" }} spacing={3}>
+        <Stack alignItems={"center"} spacing={3}>
+          <Box
+            sx={{
+              backgroundColor: theme.palette.primary.main,
+              height: 64,
+              width: 64,
+              borderRadius: 1.5,
+            }}
+          >
+            <img src={Logo} alt={"Chat app logo"} />
+          </Box>
+          <Stack sx={{ width: "max-content" }} direction="column" alignItems={"center"} spacing={3}>
+            {Nav_Buttons.map((el, index) =>
+              el.index === selected ? (
+                <Box key={index} sx={{ backgroundColor: theme.palette.primary.main, borderRadius: 1.5 }}>
+                  <IconButton sx={{ width: "max-content", color: "#fff" }} key={el.index}>
                     {el.icon}
-                  </IconButton>
-                )
-              )}
-              <Divider sx={{ width: "50px" }} />
-              {selected === 3 ? (
-                <Box sx={{ backgroundColor: theme.palette.primary.main, borderRadius: 1.5 }}>
-                  <IconButton sx={{ width: "max-content", color: "#fff" }}>
-                    <Gear />
                   </IconButton>
                 </Box>
               ) : (
                 <IconButton
                   onClick={() => {
-                    navigate(getPath(3));
-                    setSelected(3);
-                    
+                    setSelected(el.index);
+                    navigate(getPath(el.index));
+
                   }}
                   sx={{
                     width: "max-content",
                     color: theme.palette.mode === "Light" ? "#000" : theme.palette.text.primary,
                   }}
+                  key={el.index}
                 >
+                  {el.icon}
+                </IconButton>
+              )
+            )}
+            <Divider sx={{ width: "50px" }} />
+            {selected === 3 ? (
+              <Box sx={{ backgroundColor: theme.palette.primary.main, borderRadius: 1.5 }}>
+                <IconButton sx={{ width: "max-content", color: "#fff" }}>
                   <Gear />
                 </IconButton>
-              )}
-            </Stack>
-          </Stack>
-          <Stack spacing={5}>
-            <AntSwitch
-              onChange={() => {
-                onToggleMode();
-              }}
-              defaultChecked
-            />
-            <Avatar id="basic-button"
-                aria-controls={open ? 'basic-menu' : undefined}
-                aria-haspopup="true"
-                aria-expanded={open ? 'true' : undefined}
-                onClick={handleClick}  src={faker.image.animals()} />
-            <Menu
-                id="basic-menu"
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
-                MenuListProps={{
-                    'aria-labelledby': 'basic-button',
-                }}
-                anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'right',
-                }}
-                transformOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'left',
+              </Box>
+            ) : (
+              <IconButton
+                onClick={() => {
+                  navigate(getPath(3));
+                  setSelected(3);
 
                 }}
-            >
-                <Stack spacing={1} px={1}>
-                    {Profile_Menu.map((el) => (
-                        <MenuItem 
-                        onClick={handleClick} > 
-                        <Stack sx={{width : 100}} direction={"row"} alignItems={"center"} justifyContent="space-between"> 
-                          <span>{el.title}</span>
-                          {el.icon}
-                        </Stack> 
-                        </MenuItem>
-                    ))}
-                </Stack>
-
-            </Menu>
+                sx={{
+                  width: "max-content",
+                  color: theme.palette.mode === "Light" ? "#000" : theme.palette.text.primary,
+                }}
+              >
+                <Gear />
+              </IconButton>
+            )}
           </Stack>
         </Stack>
-      </Box>
-         )
+        <Stack spacing={5}>
+          <AntSwitch
+            onChange={() => {
+              onToggleMode();
+            }}
+            defaultChecked
+          />
+          <Avatar id="basic-button"
+            aria-controls={open ? 'basic-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+            onClick={handleClick} src={faker.image.animals()} />
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'basic-button',
+            }}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'right',
+            }}
+            transformOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left',
+
+            }}
+          >
+            <Stack spacing={1} px={1}>
+              {Profile_Menu.map((el, idx) => (
+                <MenuItem
+                  onClick={() => {
+                    handleClick();
+                  }} >
+                  <Stack
+
+                    onClick={() => {
+                      navigate(getMenuPath(idx))
+                    }}
+
+
+                    sx={{ width: 100 }} direction={"row"} alignItems={"center"} justifyContent="space-between">
+                    <span>{el.title}</span>
+                    {el.icon}
+                  </Stack>
+                </MenuItem>
+              ))}
+            </Stack>
+
+          </Menu>
+        </Stack>
+      </Stack>
+    </Box>
+  )
 }
 
 export default Sidebar;
--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,26 +1,31 @@
-import { Box, Stack } from "@mui/material"
+
+import { Box, Stack } from "@mui/material";
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
+
+const isAuthenticated = true;
 
 const DashboardLayout = () => {
 
+  if (!isAuthenticated) {
+    return <Navigate to="/auth/login" />;
+  }
+
   return (
     <Stack direction="row"> 
-    <Box
-      sx={{
-        display: "flex",
-        height: "100vh",
-      }}
-    >
-      {/* Sidebar */}
-      <Sidebar />
-      <Outlet />
-    </Box>
+      <Box
+        sx={{
+          display: "flex",
+          height: "100vh",
+        }}
+      >
+        {/* Sidebar */}
+        <Sidebar />
+        <Outlet />
+      </Box>
     </Stack>
-
   );
-
 };
 
 export default DashboardLayout;
