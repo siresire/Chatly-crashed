623dd6851c8dda21d10423a3bdea13b22df06ae7
Godfrey Bosire <godfreybosire@Godfreys-MacBook-Pro.local>
2023-10-27 23:39:36 +0300

    friends-logic

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,19 +1,73 @@
 
 import { Box, Stack } from "@mui/material";
-import React from "react";
-import {useSelector} from "react-redux";
+import React, { useEffect } from "react";
+import {useDispatch, useSelector} from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
+import { connectSocket, socket } from "../../sockets";
+import { showSnackbar } from "../../redux/slices/app";
 
 // const isAuthenticated = false;
 
 const DashboardLayout = () => {
 
+  const dispatch = useDispatch();
+
   const { isLoggedIn } = useSelector((state) => state.auth);
+
+  const user_id = window.localStorage.getItem("user_id");
+  
+  useEffect(() => {
+    if (isLoggedIn) {
+      window.onload = function () {
+        if (!window.location.hash) {
+          window.location = window.location + "#loaded";
+          window.location.reload();
+        }};   
+
+        window.onload();
+
+        if (!socket) {
+          connectSocket(user_id);
+        }
+
+        // new friend request listener 
+        socket.on("new_friend_request", (data) => {
+          dispatch(
+            showSnackbar({
+              severity: "success",
+              message: "New friend request received",
+            })
+          );
+        });
+
+        socket.on("request_accepted", (data) => {
+          dispatch(
+            showSnackbar({
+              severity: "success",
+              message: "Friend Request Accepted",
+            })
+          );
+        });
+
+        socket.on("request_sent", (data) => {
+          dispatch(showSnackbar({ severity: "success", message: data.message }));
+        });
+  
+    }
+    // Remove event listener on component unmount
+    return () => {
+      socket?.off("new_friend_request");
+      socket?.off("request_accepted");
+      socket?.off("request_sent");
+    };
+  }, [isLoggedIn, socket]);
 
   if (!isLoggedIn) {
     return <Navigate to="/auth/login" />;
   }
+
+
 
 
   return (
