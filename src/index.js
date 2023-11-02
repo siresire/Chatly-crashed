b166a592cb2e7c7e9830177bbacfef04622747b5
Godfrey Bosire <godfreybosire@Godfreys-MacBook-Pro.local>
2023-11-02 20:42:15 +0300

    #

--- src/index.js
+++ src/index.js
@@ -1,28 +1,25 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from 'react-router-dom';
-import { HelmetProvider } from "react-helmet-async";
+import ReactDOM from "react-dom";
+import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import persistStore from "redux-persist/es/persistStore";
+import store from "./store";
 
-// contexts
-import SettingsProvider from "./contexts/SettingsContext";
-import { store } from "./redux/store";
-import { Provider as ReduxProvider } from "react-redux";
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const persistedStore = persistStore(store);
 
-root.render(
-  <React.StrictMode>
-    <HelmetProvider>
-  <ReduxProvider store={store}>
-      <SettingsProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </SettingsProvider>
-      </ReduxProvider>
-    </HelmetProvider>
-  </React.StrictMode>
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistedStore}>
+                <App />
+            </PersistGate>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
